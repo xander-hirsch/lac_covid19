0c6830be402fde5e98956f1ff4719d825841c06c
Xander Hirsch <ahirsch@hmc.edu>
2020-03-25 12:18:27 -0700

    Implemented fetch pr from URL

--- la_public_health_pr.py
+++ la_public_health_pr.py
@@ -1,6 +1,7 @@
 import re
 
 import bs4
+import requests
 
 LAC_DPH_PR_URL_BASE = 'http://www.publichealth.lacounty.gov/phcommon/public/media/mediapubhpdetail.cfm?prid='
 
@@ -8,3 +9,10 @@ HEADER_CASES_COUNT = re.compile('^Laboratory Confirmed Cases')
 HEADER_AGE_GROUP = re.compile('^Age Group (Los Angeles County Cases Only-excl LB and Pas)')
 HEADER_MED_ATTN = re.compile('^Hospitalization and Death (among Investigated Cases)')
 HEADER_PLACE = re.compile('^CITY / COMMUNITY')
+
+
+def fetch_press_release(id):
+    r = requests.get(LAC_DPH_PR_URL_BASE + str(id))
+    if r.status_code == 200:
+        return bs4.BeautifulSoup(r.text, 'html.parser')
+    raise requests.exceptions.ConnectionError('Cannot retrieve the PR statement')
