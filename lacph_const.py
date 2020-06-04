673577eaec536b9ae4260ed3f5f37a364821b236
Xander Hirsch <ahirsch@hmc.edu>
2020-06-03 21:07:34 -0700

    Moved common constants over

---
+++ lacph_const.py
@@ -0,0 +1,20 @@
+TOTAL = 'Total'
+
+MALE = 'Male'
+FEMALE = 'Female'
+OTHER = 'Other'
+
+CITY = 'City'
+LOS_ANGELES = 'Los Angeles'
+UNINC = 'Unincorporated'
+
+DATE = 'Date'
+CASES = 'Cases'
+DEATHS = 'Deaths'
+AGE_GROUP = 'Age'
+GENDER = 'Gender'
+RACE = 'Race/Ethnicity'
+HOSPITALIZATIONS = 'Hospitalizations'
+LOCATIONS = 'Locations'
+
+TOTAL_HOSPITALIZATIONS = 'Hospitalized (Ever)'
