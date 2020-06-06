dfd488701c52966841e8ca1bdce77cabff376f8e
Xander Hirsch <ahirsch@hmc.edu>
2020-06-06 10:54:13 -0700

    Changed const naming

--- analyze_lacph_daily.py
+++ analyze_lacph_daily.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-import gla_covid_19.lacph_const as lacph_const
+import gla_covid_19.const as const
 import gla_covid_19.lacph_prid as lacph_prid
 import gla_covid_19.scrape_lacph_daily as scrape_lacph_daily
 
@@ -19,23 +19,23 @@ def access_generic(*key_name):
 
 
 def access_date(pr_stats):
-    return pd.to_datetime(pr_stats[lacph_const.DATE], unit='D')
+    return pd.to_datetime(pr_stats[const.DATE], unit='D')
 
 
 def date_limits(date_series):
-    return (date_series[lacph_const.DATE].min(), date_series[lacph_const.DATE].max())
+    return (date_series[const.DATE].min(), date_series[const.DATE].max())
 
 
 def make_df_dates(pr_stats):
     data = {
-        lacph_const.DATE:
-            map(lambda x: pd.to_datetime(x[lacph_const.DATE]), pr_stats),
-        lacph_const.CASES:
-            map(lambda x: x[lacph_const.CASES], pr_stats),
-        lacph_const.HOSPITALIZATIONS:
-            map(lambda x: x[lacph_const.HOSPITALIZATIONS], pr_stats),
-        lacph_const.DEATHS:
-            map(lambda x: x[lacph_const.DEATHS], pr_stats)
+        const.DATE:
+            map(lambda x: pd.to_datetime(x[const.DATE]), pr_stats),
+        const.CASES:
+            map(lambda x: x[const.CASES], pr_stats),
+        const.HOSPITALIZATIONS:
+            map(lambda x: x[const.HOSPITALIZATIONS], pr_stats),
+        const.DEATHS:
+            map(lambda x: x[const.DEATHS], pr_stats)
     }
     return pd.DataFrame(data)
 
@@ -44,10 +44,10 @@ def single_day_race(pr_stats):
     recorded_races = set()
     indiv_race = []
 
-    CASES_BY_RACE = lacph_const.stat_by_group(lacph_const.CASES,
-                                              lacph_const.RACE)
-    DEATHS_BY_RACE = lacph_const.stat_by_group(lacph_const.DEATHS,
-                                               lacph_const.RACE)
+    CASES_BY_RACE = const.stat_by_group(const.CASES,
+                                              const.RACE)
+    DEATHS_BY_RACE = const.stat_by_group(const.DEATHS,
+                                               const.RACE)
 
     for race in pr_stats[CASES_BY_RACE].keys():
         recorded_races.add(race)
@@ -58,10 +58,10 @@ def single_day_race(pr_stats):
     if recorded_races:
         for race in recorded_races:
             data = {
-                lacph_const.DATE: pd.to_datetime(pr_stats[lacph_const.DATE]),
-                lacph_const.RACE: race,
-                lacph_const.CASES: pr_stats[CASES_BY_RACE].get(race, np.nan),
-                lacph_const.DEATHS: pr_stats[DEATHS_BY_RACE].get(race, np.nan)
+                const.DATE: pd.to_datetime(pr_stats[const.DATE]),
+                const.RACE: race,
+                const.CASES: pr_stats[CASES_BY_RACE].get(race, np.nan),
+                const.DEATHS: pr_stats[DEATHS_BY_RACE].get(race, np.nan)
             }
             indiv_race.append(pd.DataFrame(data, index=(0,)))
 
@@ -74,44 +74,44 @@ def make_by_race(pr_stats):
     per_day = tuple(map(lambda x: single_day_race(x), pr_stats))
     per_day = tuple(filter(lambda x: x is not None, per_day))
     df = pd.concat(per_day, ignore_index=True)
-    df[lacph_const.RACE] = df[lacph_const.RACE].astype('category')
+    df[const.RACE] = df[const.RACE].astype('category')
     return df
 
 
 def make_loc_ts(pr_stats, loc_type, loc_name):
     num_copies = len(pr_stats)
     data = {
-        lacph_const.DATE:
-            map(lambda x: pd.to_datetime(x[lacph_const.DATE]), pr_stats),
-        lacph_const.LOC_CAT: (loc_type,) * num_copies,
-        lacph_const.LOC_NAME: (loc_name,) * num_copies,
-        lacph_const.CASES:
-            map(lambda x: x[lacph_const.LOCATIONS][loc_type][loc_name][0], pr_stats),
-        lacph_const.CASES_NORMALIZED:
-            map(lambda x: x[lacph_const.LOCATIONS][loc_type][loc_name][1], pr_stats)
+        const.DATE:
+            map(lambda x: pd.to_datetime(x[const.DATE]), pr_stats),
+        const.LOC_CAT: (loc_type,) * num_copies,
+        const.LOC_NAME: (loc_name,) * num_copies,
+        const.CASES:
+            map(lambda x: x[const.LOCATIONS][loc_type][loc_name][0], pr_stats),
+        const.CASES_NORMALIZED:
+            map(lambda x: x[const.LOCATIONS][loc_type][loc_name][1], pr_stats)
     }
     return pd.DataFrame(data)
 
 
 def make_by_age(pr_stats):
-    CASES_BY_AGE = lacph_const.stat_by_group(lacph_const.CASES,
-                                             lacph_const.AGE_GROUP)
+    CASES_BY_AGE = const.stat_by_group(const.CASES,
+                                             const.AGE_GROUP)
     data = {
-        lacph_const.DATE: pd.to_datetime(tuple(map(lambda x: x[lacph_const.DATE], pr_stats))),
-        lacph_const.AGE_0_17: map(lambda x: x[CASES_BY_AGE][lacph_const.AGE_0_17], pr_stats),
-        lacph_const.AGE_18_40: map(lambda x: x[CASES_BY_AGE][lacph_const.AGE_18_40], pr_stats),
-        lacph_const.AGE_41_65: map(lambda x: x[CASES_BY_AGE][lacph_const.AGE_41_65], pr_stats),
-        lacph_const.AGE_OVER_65: map(lambda x: x[CASES_BY_AGE][lacph_const.AGE_OVER_65], pr_stats)
+        const.DATE: pd.to_datetime(tuple(map(lambda x: x[const.DATE], pr_stats))),
+        const.AGE_0_17: map(lambda x: x[CASES_BY_AGE][const.AGE_0_17], pr_stats),
+        const.AGE_18_40: map(lambda x: x[CASES_BY_AGE][const.AGE_18_40], pr_stats),
+        const.AGE_41_65: map(lambda x: x[CASES_BY_AGE][const.AGE_41_65], pr_stats),
+        const.AGE_OVER_65: map(lambda x: x[CASES_BY_AGE][const.AGE_OVER_65], pr_stats)
     }
     return pd.DataFrame(data)
 
 
 def make_by_gender(pr_stats):
-    CASES_BY_GENDER = lacph_const.stat_by_group(lacph_const.CASES, lacph_const.GENDER)
+    CASES_BY_GENDER = const.stat_by_group(const.CASES, const.GENDER)
     data = {
-        lacph_const.DATE: pd.to_datetime(tuple(map(lambda x: x[lacph_const.DATE], pr_stats))),
-        lacph_const.MALE: map(lambda x: x[CASES_BY_GENDER][lacph_const.MALE], pr_stats),
-        lacph_const.FEMALE: map(lambda x: x[CASES_BY_GENDER][lacph_const.FEMALE], pr_stats)
+        const.DATE: pd.to_datetime(tuple(map(lambda x: x[const.DATE], pr_stats))),
+        const.MALE: map(lambda x: x[CASES_BY_GENDER][const.MALE], pr_stats),
+        const.FEMALE: map(lambda x: x[CASES_BY_GENDER][const.FEMALE], pr_stats)
     }
     return pd.DataFrame(data)
 
@@ -121,7 +121,7 @@ if __name__ == "__main__":
                       lacph_prid.DAILY_STATS))
 
     df = make_df_dates(all_dates)
-    burbank = make_loc_ts(all_dates, lacph_const.CITY, 'Burbank')
+    burbank = make_loc_ts(all_dates, const.CITY, 'Burbank')
 
     last_week = all_dates[56:63]
     june_1 = all_dates[63]
