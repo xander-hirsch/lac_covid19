b0020286c2b1ba95b476e2292df1c84f5c2c22db
Xander Hirsch <ahirsch@hmc.edu>
2020-06-07 17:19:10 -0700

    Added cities to region map

--- const.py
+++ const.py
@@ -50,3 +50,119 @@ CASES_BY_RACE = stat_by_group(CASES, RACE)
 DEATHS_BY_RACE = stat_by_group(DEATHS, RACE)
 CASES_BY_AGE = stat_by_group(CASES, AGE_GROUP)
 CASES_BY_GENDER = stat_by_group(CASES, GENDER)
+
+REGION = {
+    'Angeles Forest': (),
+    'Antelope Valley': (
+        (CITY, 'Lancaster'),
+        (CITY, 'Palmdale')
+    ),
+    'Central LA': (
+        (CITY, 'West Hollywood')
+    ),
+    'Eastside': (),
+    'Harbor': (
+        (CITY, 'Avalon'),
+        (CITY, 'Carson'),
+        (CITY, 'Hawaiian Gardens'),
+        (CITY, 'Lakewood'),
+        (CITY, 'Signal Hill')
+    ),
+    'Northeast LA': (),
+    'Northwest County': (
+        (CITY, 'Santa Clarita')
+    ),
+    'Pomona Valley': (
+        (CITY, 'Claremont'),
+        (CITY, 'La Verne'),
+        (CITY, 'Pomona')
+    ),
+    'San Fernando Valley': (
+        (CITY, 'Burbank'),
+        (CITY, 'San Fernando')
+    ),
+    'San Gabriel Valley': (
+        (CITY, 'Alhambra'),
+        (CITY, 'Arcadia'),
+        (CITY, 'Azusa'),
+        (CITY, 'Baldwin Park'),
+        (CITY, 'Bradbury'),
+        (CITY, 'Covina'),
+        (CITY, 'Diamond Bar'),
+        (CITY, 'Duarte'),
+        (CITY, 'El Monte'),
+        (CITY, 'Glendora'),
+        (CITY, 'Industry'),
+        (CITY, 'Irwindale'),
+        (CITY, 'La Habra Heights'),
+        (CITY, 'La Puente'),
+        (CITY, 'Monrovia'),
+        (CITY, 'Monterey Park'),
+        (CITY, 'Rosemead'),
+        (CITY, 'San Dimas'),
+        (CITY, 'San Gabriel'),
+        (CITY, 'San Marino'),
+        (CITY, 'Sierra Madre'),
+        (CITY, 'South El Monte'),
+        (CITY, 'South Pasadena'),
+        (CITY, 'Temple City'),
+        (CITY, 'Walnut'),
+        (CITY, 'West Covina'),
+        (CITY, 'Whittier')
+    ),
+    'Santa Monica Mountains': (
+        (CITY, 'Agoura Hills'),
+        (CITY, 'Calabasas'),
+        (CITY, 'Hidden Hills'),
+        (CITY, 'Malibu'),
+        (CITY, 'Westlake Village')
+    ),
+    'South Bay': (
+        (CITY, 'El Segundo'),
+        (CITY, 'Gardena'),
+        (CITY, 'Hawthorne'),
+        (CITY, 'Hermosa Beach'),
+        (CITY, 'Inglewood'),
+        (CITY, 'Lawndale'),
+        (CITY, 'Lomita'),
+        (CITY, 'Manhattan Beach'),
+        (CITY, 'Palos Verdes Estates'),
+        (CITY, 'Rancho Palos Verdes'),
+        (CITY, 'Redondo Beach'),
+        (CITY, 'Rolling Hills'),
+        (CITY, 'Rolling Hills Estates'),
+        (CITY, 'Torrance')
+    ),
+    'South LA': (),
+    'Southeast': (
+        (CITY, 'Artesia'),
+        (CITY, 'Bell'),
+        (CITY, 'Bell Gardens'),
+        (CITY, 'Bellflower'),
+        (CITY, 'Cerritos'),
+        (CITY, 'Commerce'),
+        (CITY, 'Compton'),
+        (CITY, 'Cudahy'),
+        (CITY, 'Downey'),
+        (CITY, 'Huntington Park'),
+        (CITY, 'La Mirada'),
+        (CITY, 'Lynwood'),
+        (CITY, 'Maywood'),
+        (CITY, 'Montebello'),
+        (CITY, 'Norwalk'),
+        (CITY, 'Paramount'),
+        (CITY, 'Pico Rivera'),
+        (CITY, 'Santa Fe Springs'),
+        (CITY, 'South Gate'),
+        (CITY, 'Vernon')
+    ),
+    'Verdugos': (
+        (CITY, 'Glendale'),
+        (CITY, 'La Canada Flintridge')
+    ),
+    'Westside': (
+        (CITY, 'Beverly Hills'),
+        (CITY, 'Culver City'),
+        (CITY, 'Santa Monica')
+    )
+}
