de80a50f096fb547bba1b2161842e77b5d15ea44
Xander Hirsch <ahirsch@hmc.edu>
2020-06-11 17:57:23 -0700

    Finished parsing population

--- external_data.py
+++ external_data.py
@@ -1,35 +1,54 @@
 import os.path
+import re
 
 import pandas as pd
+
+import lac_covid19.const as const
+
+AREA = const.AREA
+POPULATION = const.POPULATION
+MALE = const.MALE
+FEMALE = const.FEMALE
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 LAC_POPULATION = os.path.join(DATA_DIR, 'lac-population.csv')
 
+RE_CITY = re.compile('(.+) city')
+RE_LA = re.compile('Los Angeles city - (.+)')
+RE_UNIC = re.compile('Unincorporated County - (.+)')
+
+
+def rename_area(orig_name: str) -> str:
+    los_angeles = RE_LA.match(orig_name)
+    if los_angeles:
+        return 'Los Angeles - {}'.format(los_angeles.group(1))
+
+    unic = RE_UNIC.match(orig_name)
+    if unic:
+        return 'Unincorporated - {}'.format(unic.group(1))
+
+    city = RE_CITY.match(orig_name)
+    if city:
+        return 'City of {}'.format(city.group(1))
+
+    return None
+
 
 def process_population() -> pd.DataFrame:
     cityname = 'CITYNAME'
-    service_area = 'Service_Area'
-    male = 'Male'
-    female = 'Female'
-    residents = 'residents'
 
     df_census = (pd.read_csv(LAC_POPULATION)
-                 [[cityname, service_area, male, female]])
+                 [[cityname, MALE, FEMALE]])
     df_census[cityname] = df_census[cityname].astype('string')
 
-    unique_areas = tuple(df_census.groupby([cityname, service_area])
-                         .groups.keys())
-    df_sa = pd.DataFrame(unique_areas, columns=(cityname, service_area))
-    df_sa[cityname] = df_sa[cityname].astype('string')
-    df_sa[service_area] = df_sa[service_area].astype('category')
-    
-    df_census[residents] = df_census[male] + df_census[female]
-    df_area_pop = df_census[[cityname, residents]]
-    df_area_pop = df_area_pop.groupby(cityname).sum().reset_index()
-    df_area_pop[cityname] = df_area_pop[cityname].astype('string')
+    df_census[POPULATION] = df_census[MALE] + df_census[FEMALE]
+    df_census.drop(columns=[MALE, FEMALE], inplace=True)
+    df_census = df_census.groupby(cityname).sum().reset_index()
 
-    return df_sa, df_area_pop
+    df_census[AREA] = df_census[cityname].apply(rename_area).astype('string')
+
+    return df_census[[AREA, POPULATION]]
 
 if __name__ == "__main__":
-    sa, pop = process_population()
+    area_pop = process_population()
     df_census = pd.read_csv(LAC_POPULATION)
