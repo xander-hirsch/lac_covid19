dc1df4f442812476ccf04e715a3d34eea6b4f540
Xander Hirsch <ahirsch@hmc.edu>
2020-07-14 07:08:04 -0700

    Improved phone alert logging
    alert_phone() now logs its calling and the
    REST API response directory is created if
    it does not exist

--- lacph-monitor/helper.sh
+++ lacph-monitor/helper.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 source $HOME/.pushbullet_vars
+PROJECT_DIR=$(dirname $(readlink --canonicalize $0))
+
 LOGFILE="$PROJECT_DIR/monitor.log"
+DIR_PUSHBULLET_RESP="$PROJECT_DIR/pushbullet-log"
 
 CHANGE='Page changed'
 NO_CHANGE='No change'
 
+function write_log() {
+    echo "[ $(date -Iminutes) ] $1: $2" >> $LOGFILE
+}
+
 function alert_phone() {
-    OUTPUT="$PROJECT_DIR/pushbullet-log/$(date -Iminutes).json"
     # OUTPUT='/dev/null'
+
+    OUTPUT="$DIR_PUSHBULLET_RESP/$(date -Iminutes).json"
+    if [ ! -d $DIR_PUSHBULLET_RESP ]
+    then
+        mkdir $DIR_PUSHBULLET_RESP
+    fi
 
     curl --silent --output $OUTPUT \
         --request POST 'https://api.pushbullet.com/v2/pushes' \
@@ -17,8 +29,6 @@ function alert_phone() {
         --form 'type=note' \
         --form 'title=LA Public Health Update' \
         --form "body=$1"
-}
 
-function write_log() {
-    echo "[ $(date -Iminutes) ] $1: $2" >> $LOGFILE
+    write_log 'Pushbullet API' "Sent '$1'"
 }
