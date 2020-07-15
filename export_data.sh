2b4279958cc9f7c9fc6499258e65d24d96c4bf0f
Xander Hirsch <ahirsch@hmc.edu>
2020-07-14 20:13:16 -0700

    Found ways to improve data sripts

--- export_data.sh
+++ export_data.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-DIR_DATA="./append-data";
-FOLDER_DATA_EXPORT="lac-covid19-data-append"
-FILE_DATA_EXPORT="$FOLDER_DATA_EXPORT.zip";
+DIR_DATA='./append-data'
+FOLDER_DATA_EXPORT='lac-covid19-data-append'
+FILE_DATA_EXPORT="$FOLDER_DATA_EXPORT.zip"
 
-cd $DIR_DATA;
+cd $DIR_DATA
+
 zip $FILE_DATA_EXPORT *.csv \
     && mv $FILE_DATA_EXPORT $W10_DATA \
-    && rm -rf "$W10_DATA/$FOLDER_DATA_EXPORT";
+    && rm -rf "$W10_DATA/$FOLDER_DATA_EXPORT"
--- update_html.sh
+++ update_html.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-IPYNB_NAME="lacph_graphs"
-W10_HTML="$W10_DOWNLOADS/$IPYNB_NAME.html";
-HTML_PAGE="./docs/index.html";
+IPYNB_NAME='lacph_graphs'
+W10_HTML="$W10_DOWNLOADS/$IPYNB_NAME.html"
+REPO_HTML='./docs/index.html'
 
-# jupyter notebook $LAC_IPYNB;
+jupyter notebook "$IPYNB_NAME.ipynb"
 
-cp $W10_HTML $HTML_PAGE && rm $W10_HTML;
-chmod 644 $HTML_PAGE;
-
-git add $HTML_PAGE;
+if [ $W10_HTML -nt $REPO_HTML ]
+then
+    mv $W10_HTML $REPO_HTML && chmod 644 $REPO_HTML
+fi
