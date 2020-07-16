1f45af1f628ac873d3dd3445a3f768df5a776241
Xander Hirsch <ahirsch@hmc.edu>
2020-07-15 23:45:50 -0700

    Added stats checker removed education checker
    Implemented the means to check the case stats page for changes.
    The K-12 documents were modified in a way which was not caught by
    the previous checker. More utility added to the helper script.

--- lacph-monitor/helper.sh
+++ lacph-monitor/helper.sh
@@ -6,8 +6,49 @@ PROJECT_DIR=$(dirname $(readlink --canonicalize $0))
 LOGFILE="$PROJECT_DIR/monitor.log"
 DIR_PUSHBULLET_RESP="$PROJECT_DIR/pushbullet-log"
 
+SUSPEND_FILE='.suspend_'
+
 CHANGE='Page changed'
 NO_CHANGE='No change'
+
+function suspend_monitor() {
+    touch $1
+}
+
+function check_monitor() {
+    if [ -f $1 ]
+    then
+        exit
+    fi
+}
+
+LYNX_OPTS='-dump -nonumbers -nolist -display_charset=utf-8'
+function request_page() {
+    lynx $LYNX_OPTS $1 > $2
+}
+
+function cli_opts() {
+    # 1 - cli argument; 2 - URL; 3 - save file; 4 - suspend file
+    msg=
+    case $1 in
+        'init')
+            request_page $2 $3
+            msg='Monitor initialized'
+            ;;
+        'suspend')
+            suspend_monitor $4
+            msg='Monitor suspended'
+            ;;
+        'reset')
+            rm $4
+            msg='Monitor reset'
+            ;;
+        *)
+            msg='Bad option'
+    esac
+    echo "$msg"
+    exit
+}
 
 function write_log() {
     echo "[ $(date -Iminutes) ] $1: $2" >> $LOGFILE
