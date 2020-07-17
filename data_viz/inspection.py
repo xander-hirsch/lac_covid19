68dd30ba3028451dd5d129406ca769d602fc4fa5
Xander Hirsch <ahirsch@hmc.edu>
2020-07-16 17:32:28 -0700

    Press release update for Thursday, 16 July

--- data_viz/inspection.py
+++ data_viz/inspection.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[19]:
 
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -20,7 +21,7 @@ df_csa_curr = pd.read_pickle(paths.CSA_CURRENT_PICKLE)
 
 # ## Choropleth Values
 
-# In[2]:
+# In[24]:
 
 
 covid_plots.csa_distribution(df_csa_curr, lac_regions.ALL_CSA, const.CASE_RATE)
@@ -68,6 +69,38 @@ df_csa_ts = pd.read_pickle(paths.CSA_TS_PICKLE)
 
 area = lac_regions.ALL_CSA[25]
 covid_plots.csa_ts(df_csa_ts, area)
+
+
+# ## Rolling Average
+
+# In[26]:
+
+
+df_summary = pd.read_pickle(paths.MAIN_STATS_PICKLE)
+
+df_summary['Avg New Cases'] = df_summary[const.NEW_CASES].rolling(3).mean()
+df_summary['New Cases Predictor'] = df_summary['Avg New Cases'].transform(np.log)
+
+df_summary['Total Cases Predictor'] = df_summary[const.CASES].dropna().transform(np.log)
+
+sns.scatterplot(x=const.DATE, y='Total Cases Predictor', data=df_summary)
+plt.xlim((pd.Timestamp(2020, 3, 30), pd.Timestamp(2020, 7, 16)))
+plt.xticks(rotation='45')
+plt.show()
+
+ax = sns.scatterplot(x=const.DATE, y='Avg New Cases', data=df_summary)
+plt.xlim((pd.Timestamp(2020, 3, 30), pd.Timestamp(2020, 7, 16)))
+plt.xticks(rotation='45')
+plt.title('New COVID-19 Cases in LA County over Time')
+plt.ylabel('3 Day Rolling Average')
+plt.show()
+
+ax = sns.scatterplot(x=const.DATE, y='Predictor', data=df_summary)
+plt.xlim((pd.Timestamp(2020, 3, 30), pd.Timestamp(2020, 7, 16)))
+plt.xticks(rotation='45')
+plt.title('New COVID-19 Cases in LA County over Time')
+plt.ylabel('ln(3 Day Rolling Average)')
+plt.show()
 
 
 # In[ ]:
