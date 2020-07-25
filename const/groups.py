4a6e841c6f20adfad72d754f063219a2547bf3b4
Xander Hirsch <ahirsch@hmc.edu>
2020-07-25 13:39:28 -0700

    New age group constants

--- const/groups.py
+++ const/groups.py
@@ -1,7 +1,17 @@
-AGE_0_17 = '0 to 17'
-AGE_18_40 = '18 to 40'
-AGE_41_65 = '41 to 65'
-AGE_OVER_65 = 'over 65'
+_AGE_RANGE = '{} to {}'
+_AGE_OVER = 'over {}'
+AGE_0_17 = _AGE_RANGE.format(0, 17)
+AGE_18_40 = _AGE_RANGE.format(18, 40)
+AGE_41_65 = _AGE_RANGE.format(41, 65)
+AGE_OVER_65 = _AGE_OVER.format(65)
+AGE_0_4 = _AGE_RANGE.format(0, 4)
+AGE_5_11 = _AGE_RANGE.format(5, 11)
+AGE_12_17 = _AGE_RANGE.format(12, 17)
+AGE_18_29 = _AGE_RANGE.format(18, 29)
+AGE_30_49 = _AGE_RANGE.format(30, 49)
+AGE_50_64 = _AGE_RANGE.format(50, 64)
+AGE_65_79 = _AGE_RANGE.format(65, 79)
+AGE_OVER_80 = _AGE_OVER.format(80)
 
 MALE = 'Male'
 FEMALE = 'Female'
