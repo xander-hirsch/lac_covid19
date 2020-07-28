0ba3a3e3a7e9cfa4f255c5ed89d166a39602b0d3
Xander Hirsch <ahirsch@hmc.edu>
2020-07-27 17:46:54 -0700

    Renamed module daily_pr.analyze_lacph_daily to generate_time_series

--- data_mgmt.py
+++ data_mgmt.py
@@ -11,7 +11,7 @@ import lac_covid19.covid_plots as covid_plots
 import lac_covid19.current_stats.scrape_current as scrape_current
 import lac_covid19.daily_pr.bad_data as bad_data
 import lac_covid19.daily_pr.scrape_daily_stats as scrape_daily_stats
-import lac_covid19.daily_pr.analyze_lacph_daily as analyze_lacph_daily
+import lac_covid19.daily_pr.generate_time_series as generate_time_series
 import lac_covid19.geo as geo
 import lac_covid19.geo.csa as geo_csa
 
@@ -40,13 +40,13 @@ def new_daily_pr(date_: str = None):
 def export_time_series():
     to_export = (
         (paths.MAIN_STATS_CSV, paths.MAIN_STATS_PICKLE,
-         analyze_lacph_daily.create_main_stats),
+         generate_time_series.create_main_stats),
         (paths.AGE_CSV, paths.AGE_PICKLE,
-         analyze_lacph_daily.create_by_age),
+         generate_time_series.create_by_age),
         (paths.GENDER_CSV, paths.GENDER_PICKLE,
-         analyze_lacph_daily.create_by_gender),
+         generate_time_series.create_by_gender),
         (paths.RACE_CSV, paths.RACE_PICKLE,
-         analyze_lacph_daily.create_by_race),
+         generate_time_series.create_by_race),
     )
 
     all_dates = scrape_daily_stats.query_all_dates()
@@ -55,8 +55,8 @@ def export_time_series():
         df = function(all_dates)
         write_dataframe(df, csv_file, pickle_file)
 
-    df_csa = analyze_lacph_daily.create_by_area(all_dates)
-    df_region = analyze_lacph_daily.aggregate_locations(
+    df_csa = generate_time_series.create_by_area(all_dates)
+    df_region = generate_time_series.aggregate_locations(
         df_csa, bad_data.BAD_DATE_AREA)
 
     location_export = (
