2cd00a7adf6d5e7784df9da265be8e2c0d068cf7
Xander Hirsch <ahirsch@hmc.edu>
2020-07-27 19:39:18 -0700

    Moved data_viz to visualization foder

--- update_html.sh
+++ update_html.sh
@@ -4,10 +4,10 @@ IPYNB_NAME='lacph_graphs'
 W10_HTML="$W10_DOWNLOADS/$IPYNB_NAME.html"
 REPO_HTML='./docs/index.html'
 
-jupyter notebook "data_viz/$IPYNB_NAME.ipynb"
+jupyter notebook "visualization/$IPYNB_NAME.ipynb"
 
 if [ $W10_HTML -nt $REPO_HTML ]
 then
     mv $W10_HTML $REPO_HTML && chmod 644 $REPO_HTML
-    ./data_viz/git_ipynb.sh
+    ./visualization/git_ipynb.sh
 fi
