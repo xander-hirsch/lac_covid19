51949dec87cc281c22920cd4206d3aeabd40d9d6
Xander Hirsch <ahirsch@hmc.edu>
2020-07-28 12:52:36 -0700

    Drops date in current snapshot

--- analysis/current.py
+++ analysis/current.py
@@ -7,7 +7,8 @@ import lac_covid19.const.paths as paths
 def snapshot(df: pd.DataFrame) -> pd.DataFrame:
     """Produces a snapshot of the current dataset"""
     last_day = df[const.DATE].max()
-    return df[df[const.DATE] == last_day]
+    most_recent = df[df[const.DATE] == last_day]
+    return most_recent.drop(columns=const.DATE)
 
 if __name__ == "__main__":
     df_age = pd.read_pickle(paths.AGE_PICKLE)
