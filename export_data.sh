a4395252ee8c5793b121336b8393ee203c26827a
Xander Hirsch <ahirsch@hmc.edu>
2020-08-05 12:47:42 -0700

    Removed export data file rename

--- export_data.sh
+++ export_data.sh
@@ -8,9 +8,6 @@ cd $DIR_DATA
 
 cp ../docs/{current,deltas}/*.csv .
 
-# Change filename for ArcGIS compatability
-mv region-deltas.csv delta-region.csv
-
 zip $FILE_DATA_EXPORT *.csv \
     && mv $FILE_DATA_EXPORT $W10_DATA \
     && rm -rf "$W10_DATA/$FOLDER_DATA_EXPORT"
