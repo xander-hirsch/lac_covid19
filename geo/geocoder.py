6799b55b700786e3a342e6561e8feb3b0d9fc231
Xander Hirsch <ahirsch@hmc.edu>
2020-12-22 21:39:53 -0800

    Refactored geo to improve geocoding process
    A message is printed to the terminal when an address is queried online.

--- geo/geocoder.py
+++ geo/geocoder.py
@@ -19,59 +19,56 @@ APPEND_ZIP = {
 
 
 ADDRESS_CACHE_PATH = os.path.join(DIR_DATA, 'addresses.json')
-def load_addresses():
+def load_addresses_cache():
     address_dict = {}
     if os.path.isfile(ADDRESS_CACHE_PATH):
         with open(ADDRESS_CACHE_PATH) as f:
             address_dict = json.load(f)
         for address in address_dict:
             address_dict[address] = tuple(address_dict[address])
-        return address_dict
-ADDRESSES = load_addresses()
+    return address_dict
+ADDRESSES = load_addresses_cache()
 
 
-def lookup_address(address: str) -> Tuple[float, float]:
+def lookup_address(address_query: str) -> Tuple[float, float]:
     # Normalize queries by saving all upper case
-    address = address.upper()
-    if address in APPEND_ZIP:
-        address = f'{address}, {APPEND_ZIP[address]}'
-    if ADDRESSES and address in ADDRESSES:
-        return ADDRESSES[address]
-    query = BING_MAPS_QUERY.geocode(address, user_location=LAC_CENTER)
+    address_query = address_query.upper()
+    if address_query in APPEND_ZIP:
+        address_query = f'{address_query}, {APPEND_ZIP[address_query]}'
+    if address_query in ADDRESSES:
+        return ADDRESSES[address_query]
+    query = BING_MAPS_QUERY.geocode(address_query, user_location=LAC_CENTER)
     resp_point = query.latitude, query.longitude
-    if ADDRESSES is not None:
-        ADDRESSES[address] = resp_point
+    ADDRESSES[address_query] = resp_point
+    print(f'{address_query}: ({resp_point[0]}, {resp_point[1]})')
     return resp_point
 
 
-def lookup_many_addresses(addresses: Iterable[str], buffer=25):
-    cache_dict = {}
-    if os.path.isfile(ADDRESS_CACHE_PATH):
-        with open(ADDRESS_CACHE_PATH) as f:
-            cache_dict = json.load(f)
+def lookup_many_addresses(many_address_queries: Iterable[str], buffer=25):
     queries_ran = 0
-    for address in addresses:
-        if address not in cache_dict:
+    for address in many_address_queries:
+        if address not in ADDRESSES:
             lookup_address(address)
             queries_ran += 1
             if queries_ran == buffer:
                 with open(ADDRESS_CACHE_PATH, 'w') as f:
-                    json.dump(cache_dict, f)
+                    json.dump(ADDRESSES, f)
                 queries_ran = 0
     if queries_ran != 0:
         with open(ADDRESS_CACHE_PATH, 'w') as f:
-            json.dump(cache_dict, f)
+            json.dump(ADDRESSES, f)
 
 
 def prep_addresses():
-    live_page = current_stats.query_live(False)
+    live_page = current_stats.query_live(True)
     addresses = (
         list(live_page[const.NON_RESIDENTIAL][const.ADDRESS])
         + list(live_page[const.EDUCATION][const.ADDRESS])
         + list(current_stats.CITATIONS[const.ADDRESS])
     )
-    addresses = set(filter(lambda x: x!='Los Angeles, CA', addresses))
-    lookup_many_addresses(addresses, ADDRESS_CACHE_PATH)
+    lookup_many_addresses(
+        set(map(lambda x: x.upper(),
+                filter(lambda x: x!='Los Angeles, CA', addresses)))
+    )
     global ADDRESSES
-    ADDRESSES = load_addresses()
-
+    ADDRESSES = load_addresses_cache()
