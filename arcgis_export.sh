f8ce3c59414cc11fc9846ce870a18437366b2cda
Xander Hirsch <ahirsch@hmc.edu>
2020-12-22 17:31:53 -0800

    Update 2020-12-22

--- arcgis_export.sh
+++ arcgis_export.sh
@@ -5,6 +5,7 @@ ZIP_FILENAME='lac-covid19-data'
 FILE_DATA_EXPORT="$ZIP_FILENAME.zip"
 
 cd $DIR_DATA
+cp ../docs/time-series/cdph-reopening.csv arcgis-upload/
 
 zip -r $FILE_DATA_EXPORT . \
     && mv $FILE_DATA_EXPORT $W10_DATA \
